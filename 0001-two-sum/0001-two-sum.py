c0d09b24d52be15a3745211105bc28d8cb44adf5
Pragada Lakshmi Venkata Kalki Sainath <110445059+sainath1754@users.noreply.github.com>
2026-07-23 22:58:12 +0530

    Time: 0 ms (100%), Space: 20.5 MB (17.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            temp=target-nums[i]
-            if (temp in nums) and (nums.index(temp)!=i):
-                return [i,nums.index(temp)]
- 
+        d={}#d={}
+        for i in range(len(nums)): #i=1 nums[i]=3
+            temp=target-nums[i]#temp=6-3=3
+            if temp in d: #3 in d
+                return [d[temp],i] # d[temp] -> value  2 's value and present i value
+            d[nums[i]]=i #d[3]=0   in dict : key-list lo value, value - index
+        return []
