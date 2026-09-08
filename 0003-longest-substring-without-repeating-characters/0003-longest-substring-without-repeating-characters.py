5e234b00d2a13477fd29fce1d6a1614891366112
Pragada Lakshmi Venkata Kalki Sainath <110445059+sainath1754@users.noreply.github.com>
2026-09-08 19:37:39 +0530

    Time: 199 ms (51.01%), Space: 20 MB (24.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,13 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #brute force approach 
-        n = len(s)
+        # #brute force approach 
+        # n = len(s)
+        # max_len=0
+        # for i in range(n):
+        #     seen=set()
+        #     for j in range(i,n):
+        #         ch=s[j]
+        #         if ch in seen:break #duplicate character in set
+        #         seen.add(ch)
+        #         max_len= max(max_len,j-i+1)
+        # return max_len
+
+        #optimized approch
+        dict = {}
+        left=0
         max_len=0
-        for i in range(n):
-            seen=set()
-            for j in range(i,n):
-                ch=s[j]
-                if ch in seen:break #duplicate character in set
-                seen.add(ch)
-                max_len= max(max_len,j-i+1)
+        n=len(s)
+        for right in range(n):
+            if s[right] in dict: #duplicate existed
+                left=max(left , dict[s[right]]+1) 
+                #abcdefeg   left will be moved to f (index=5)
+                #left is moved till the charcters next index
+            dict[s[right]]=right # new iteration right start point
+            max_len =max(max_len,right-left+1)
         return max_len
